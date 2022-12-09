043923f338dd281831a3058b8f13c70b97b3caae
Victoria Beasley <iesnoth91@gmail.com>
2022-12-08 18:40:39 -0600

    first component and route

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
+//putting Browser Router here gives Route abilities to the entire App file
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
