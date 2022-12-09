c705eac5c84de224a492b29c076f574a7b1ae581
Victoria Beasley <iesnoth91@gmail.com>
2022-12-08 19:36:21 -0600

    finished nav bar

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -4,6 +4,7 @@ import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin, faTumblr } from '@fortawesome/free-brands-svg-icons';
 
 const Sidebar = () => {
     return(
@@ -24,6 +25,23 @@ const Sidebar = () => {
                 <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"/>
             </NavLink>
         </nav>
+        <ul>
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/victoria-beasley-codes/'>
+                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e"/>
+                </a>
+            </li>
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://github.com/iesnoth'>
+                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e"/>
+                </a>
+            </li>
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://iesnoth.tumblr.com/tagged/artz'>
+                    <FontAwesomeIcon icon={faTumblr} color="#4d4d4e"/>
+                </a>
+            </li>
+        </ul>
         </div>
     )
 }
