d2fce49585b78ac542ac4ea46aeeee467aa2d6cd
Victoria Beasley <iesnoth91@gmail.com>
2022-12-08 19:58:23 -0600

    Home rendered

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 //importing sass css
 import './App.scss';
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Layout from './components/layout/Layout';
+import Home from './components/home/Home';
 
 function App() {
   return (
     <>
-    <Routes>
-    <Route path="/" element={<Layout/>}/>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Layout />} />
+        {/* index element passes App with Home rendered as a child */}
+        <Route index element={<Home/>}/>
+      </Routes>
     </>
   );
 }
--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,11 +1,26 @@
 import './Layout.scss'
 import Sidebar from '../sidebar/Sidebar';
+import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
     return (
-        <>
+        <div className='App'>
             <Sidebar />
-        </>
+            <div className='page'>
+                {/* get rif of the elements with tags in the classnames later.
+                They are only for design purposes. */}
+                <span className='tags top-tags'> &lt;body&gt;</span>
+
+                <Outlet/>
+
+                <span className='tags bottom-tag'>
+                &lt;/body&gt;
+                <br/>
+                <span className='bottom-tag-html'>&lt;/html&gt;</span>
+                </span>
+
+            </div>
+        </div>
     )
 }
 
