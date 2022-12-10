1b99a30dc1cd09c9bd892430c9ce22ca587f95b8
Victoria Beasley <iesnoth91@gmail.com>
2022-12-10 08:48:57 -0600

    art array

--- src/components/portfolio/ArtPieces.js
+++ src/components/portfolio/ArtPieces.js
@@ -1,40 +1,46 @@
 const ArtPieces = [
     {
-        title:'',
-        date:'',
-        url:'',
-        id:
+        title:'Afternoon Naps',
+        date:'2021',
+        url:'./src/assets/images/ariel-jim.jpg',
+        id:1
     },
     {
-        title:'',
-        date:'',
-        url:'',
-        id:
+        title:'Trapped in Space Game',
+        date:'2022',
+        url:'./src/assets/images/dashboard.jpg',
+        id:2
     },
     {
-        title:'',
-        date:'',
-        url:'',
-        id:
+        title:'Unfortunate College Kids cover',
+        date:'2022',
+        url:'./src/assets/images/david.jpg',
+        id:3
     },
     {
-        title:'',
-        date:'',
-        url:'',
-        id:
+        title:'Fish Statues for Client',
+        date:'2022',
+        url:'./src/assets/images/fish.jpg',
+        id:4
     },
     {
-        title:'',
-        date:'',
-        url:'',
-        id:
+        title:'Cryptid Horrors Preschool cover',
+        date:'2022',
+        url:'./src/assets/images/chp-cover.jpg',
+        id:5
     },
     {
-        title:'',
-        date:'',
-        url:'',
-        id:
+        title:'Der Tod',
+        date:'2021',
+        url:'./src/assets/images/death.jpg',
+        id:6
     },
+    {
+        title:'Fissure comic panels',
+        date:'2020',
+        url:'./src/assets/images/fissure-panel.jpg',
+        id:7
+    }
 
 ]
 
